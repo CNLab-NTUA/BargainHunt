02d1654bf5a6cb8f244b70f2fdb89a51e098aa18
zannis <zannis.kal@gmail.com>
2017-05-04 20:01:11 +0300

    added some conflicting files

---
+++ WebApp/src/main/java/gr/ntua/cn/zannis/bargains/webapp/ejb/dao/OfferDAO.java
@@ -0,0 +1,12 @@
+package gr.ntua.cn.zannis.bargains.webapp.ejb.dao;
+
+import gr.ntua.cn.zannis.bargains.webapp.persistence.entities.Offer;
+
+import java.util.List;
+
+/**
+ * @author zannis <zannis.kal@gmail.com
+ */
+public interface OfferDAO {
+    List<Offer> findTopActive(int offersToReturn);
+}
